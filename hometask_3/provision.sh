8509b98650ccea19ae09837bf7eb5383e1fa5d9c
vladoosbobrovsky <500kg.ggwp@gmail.com>
2020-02-08 17:23:53 +0200

    upd

--- hometask_3/provision.sh
+++ hometask_3/provision.sh
@@ -1,41 +1,52 @@
-sudo apt-get update -y
+#!/usr/bin/env bash
+apt-get update
+apt-get install mysql-server -y
+sudo mysql -e 'use mysql;
+
+update user set authentication_string=PASSWORD("123456") where User="root";
+flush privileges;
+CREATE DATABASE CONFLUENCE;
+quit'
+/etc/init.d/mysql start
+
 echo "-----------------------Download Atlasian Confluence -----------------------"
-#wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin 
-touch response.varfile
-echo "
-app.confHome=/var/atlassian/application-data/confluence7_3_1
-app.install.service$Boolean=false
-portChoice=custom
-httpPort$Long=8080
-rmiPort$Long=8001
-launch.application$Boolean=false
-sys.adminRights$Boolean=true
-sys.confirmedUpdateInstallationString=false
-sys.installationDir=/opt/atlassian/confluence7_3_1
-sys.languageId=en" >> response.varfile
-cd /
-chmod +x /vagrant/atlassian-confluence-7.3.1-x64.bin
-vagrant/./atlassian-confluence-7.3.1-x64.bin -q -varfile /home/vagrant/response.varfile
+#wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin
 
-touch /etc/systemd/system/confluence.service
-echo "
-[Unit]
-Description=Confluence
-After=network.target
+#echo "
+#app.confHome=/var/atlassian/application-data/confluence7_3_1
+#app.install.service$Boolean=false
+#portChoice=default
+#launch.application$Boolean=false
+#sys.adminRights$Boolean=true
+#sys.confirmedUpdateInstallationString=false
+#sys.installationDir=/opt/atlassian/confluence7_3_1
+#sys.languageId=en" >> response.varfile
+ 
+ #touch /etc/systemd/system/confluence.service
 
-[Service]
-Type=forking
-User=confluence
-PIDFile=/opt/atlassian/confluence7_3_1/work/catalina.pid
-ExecStart=/opt/atlassian/confluence7_3_1/bin/start-confluence.sh
-ExecStop=/opt/atlassian/confluence7_3_1/bin/stop-confluence.sh
-TimeoutSec=200
-LimitNOFILE=4096
-LimitNPROC=4096
+#echo "
+#[Unit]
+#Description=Confluence
+#After=network.target
 
-[Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/confluence.service
+#[Service]
+#Type=forking
+#User=confluence
+#PIDFile=/opt/atlassian/confluence7_3_1/work/catalina.pid
+#ExecStart=/opt/atlassian/confluence7_3_1/bin/start-confluence.sh
+#ExecStop=/opt/atlassian/confluence7_3_1/bin/stop-confluence.sh
+#TimeoutSec=200
+#LimitNOFILE=4096
+#LimitNPROC=4096
 
+#[Install]
+#WantedBy=multi-user.target" >> /etc/systemd/system/confluence.service
+
+cd /vagrant 
+sudo chmod +x atlassian-confluence-7.3.1-x64.bin
+./atlassian-confluence-7.3.1-x64.bin -q -varfile /vagrant/response.varfile
+cd /opt/atlassian/confluence7_3_1/bin
+./start-confluence.sh
 
 #chmod 664 /etc/systemd/system/confluence.service
 #cd /home/vagrant/
